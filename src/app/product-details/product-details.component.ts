a420d134e21ef53c3fdcc5ee6cc7d47b017aa6e8
Ruben Sanchez <r@rubensanchez.dev>
2021-04-11 22:38:12 +0200

    Initial commit

---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent implements OnInit {
+  product;
+  constructor(private route: ActivatedRoute,) {
+    
+   }
+
+  ngOnInit(): void {
+    // get product id from the current route
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    // find the product that corresponds with the product id of the route
+    this.product = products.find(product => product.id === productIdFromRoute);
+
+  }
+
+}
